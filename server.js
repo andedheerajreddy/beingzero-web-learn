8c5a2e9552881561e8afae6273c1be5de8773cea
Dheeraj  Reddy <adreddy.jmd@gmail.com>
2021-04-30 10:06:14 +0530

    added resume

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
- 
+
 const app = express();
- 
-app.get("/", function(req, res){
+
+
+app.use(express.static("frontend"));
+app.get("/", function(req, res) {
     res.send("Welcome to My Basic Site");
 })
- 
-// Heroku will automatically set an environment variable called PORT
+app.get("/resume", (req, res) => {
+        res.sendFile(__dirname + "/frontend/html/resume.html");
+    })
+    // Heroku will automatically set an environment variable called PORT
 const PORT = process.env.PORT || 3000;
- 
+
 // Start the server
-app.listen(PORT, function(){
-    console.log("Server Starting running on http://localhost:"+PORT);
+app.listen(PORT, function() {
+    console.log("Server Starting running on http://localhost:" + PORT);
 })
